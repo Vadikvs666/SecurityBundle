93ab3abe4d60e68c004d308245b4533b2c22de77
Vadim Vagin <vad.86@bk.ru>
2017-01-18 08:51:55 +0600

    Update routing.php

--- Resources/config/routing.php
+++ Resources/config/routing.php
@@ -8,15 +8,14 @@ $collection = new RouteCollection();
 $collection->add('login', new Route('/login', array(
     '_controller' => 'VVSSecurityBundle:Security:login',
 )));
-
-//$collection->add('login', new Route('/login'));
+$collection->add('login_check', new Route('/login_check'));
 $collection->add('logout', new Route('/logout'));
 
 
 
 
 
-/*добавление роутинга к администрированию пользоывателей*/
+/*добавление роутинга к администрированию пользователей и ролей*/
 
 $user=$loader->import("@VVSSecurityBundle/Resources/config/routing/user.php");
 $user->addPrefix('vvs-admin/user');
