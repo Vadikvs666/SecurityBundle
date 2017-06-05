e5a2f0c1efc06018f8c880757e6b1121a96ad88d
Vadim Vagin <vadikvs666@gmail.com>
2017-06-05 14:04:20 +0600

    mod user and routing

--- Repository/UserRepository.php
+++ Repository/UserRepository.php
@@ -2,13 +2,12 @@
 namespace VVS\SecurityBundle\Repository;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Doctrine\ORM\EntityRepository;
 
 
-class UserRepository extends EntityRepository implements UserProviderInterface , UserLoaderInterface
+class UserRepository extends EntityRepository implements UserProviderInterface 
 {
     public function loadUserByUsername($username)
     {
--- Resources/config/routing.php
+++ Resources/config/routing.php
@@ -8,8 +8,7 @@ $collection = new RouteCollection();
 $collection->add('login', new Route('/login', array(
     '_controller' => 'VVSSecurityBundle:Security:login',
 )));
-
-//$collection->add('login', new Route('/login'));
+$collection->add('login_check', new Route('/login_check'));
 $collection->add('logout', new Route('/logout'));
 
 
@@ -19,11 +18,11 @@ $collection->add('logout', new Route('/logout'));
 /*добавление роутинга к администрированию пользоывателей*/
 
 $user=$loader->import("@VVSSecurityBundle/Resources/config/routing/user.php");
-$user->addPrefix('vvs-admin/user');
+$user->addPrefix('admin/user');
 $collection->addCollection($user);
 
 $user=$loader->import("@VVSSecurityBundle/Resources/config/routing/role.php");
-$user->addPrefix('vvs-admin/role');
+$user->addPrefix('admin/role');
 $collection->addCollection($user);
 
 
